4218436f7245b4451ef5d5be3f6dd46b4d175b14
al16gm <alexgarciam91@gmail.com>
2026-04-09 12:18:34 +0200

    Update default SCRIPT_URL_DEFAULT value

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
  * config.js — Configuración de la URL del backend
  */
 
-const SCRIPT_URL_DEFAULT = 'https://script.google.com/macros/s/AKfycbwdyY32xt6lT-_tOKxN4pnIof0MlloiQnm5iJmtGHiiTh7aMnhkzoXg4TIGp8GTRy__/exec';
+const SCRIPT_URL_DEFAULT = 'https://script.google.com/macros/s/AKfycbx_cW73cmLvHr-gRgIxQY1jtZ2XohGEtZXRi3z-BBwsuPEPCBS71ulDspNKGc0AiYdR/exec';
 
 const SCRIPT_URL = localStorage.getItem('millon_script_url') || SCRIPT_URL_DEFAULT;
 
